1cca4adb0a0d7bbd062b1026e55dc1813174b128
Ralf Weber <ralf@weberr.de>
2011-02-09 15:17:37 +0000

    Initial Import

--- branches/4.0.0/site/views/rwcards/view.html.php
+++ branches/4.0.0/site/views/rwcards/view.html.php
@@ -1,26 +1,20 @@
 <?php
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
- 
+
 // import Joomla view library
 jimport('joomla.application.component.view');
- 
+
 /**
- * HTML View class for the HelloWorld Component
+ * Rwcards View
  */
 class RwcardsViewRwcards extends JView{
+
         // Overwriting JView display method
-        function display($tpl = null) 
-        {
+        function display($tpl = null){
                 // Assign data to the view
-                $this->msg = $this->get('Msg');
+                $this->msg = 'Hello World';
  
-                // Check for errors.
-                if (count($errors = $this->get('Errors'))) 
-                {
-                        JError::raiseError(500, implode('<br />', $errors));
-                        return false;
-                }
                 // Display the view
                 parent::display($tpl);
         }
