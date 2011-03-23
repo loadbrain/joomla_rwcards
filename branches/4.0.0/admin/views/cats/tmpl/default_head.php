80ddcba82f4f2e3549f62be9fe28b391d886ff0e
Ralf Weber <ralf@weberr.de>
2011-03-23 07:51:45 +0000

    Added PH1-PHP Headers missing GPL License Notice

--- branches/4.0.0/admin/views/cats/tmpl/default_head.php
+++ branches/4.0.0/admin/views/cats/tmpl/default_head.php
@@ -1,6 +1,16 @@
 <?php
-// No direct access to this file
-defined('_JEXEC') or die('Restricted Access');
+/*------------------------------------------------------------------------
+# com_rwcards4 - RWCards for Joomla 1.6
+# ------------------------------------------------------------------------
+# author Ralf Weber, LoadBrain
+# copyright (C) 2011 www.weberr.de. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://www.weberr.de
+# Technical Support: Forum - http://www.weberr.de/forum.html
+-------------------------------------------------------------------------*/
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' ); 
+
 $user		= JFactory::getUser();
 $userId		= $user->get('id');
 $listOrder	= $this->state->get('list.ordering');
