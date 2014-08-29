c8436a685f9711a9eee845e331bcd8ed4b574e39
Ralf <ralf.weber@ub.uni-freiburg.de>
2014-08-29 12:04:30 +0200

    init master

--- branches/5.0.0/admin/models/forms/cat.js
+++
@@ -1,9 +0,0 @@
-window.addEvent('domready', function(){
-
-    document.formvalidator.setHandler('categoryname', function(value){
-        regex = /[\d\w].+\s*/;
-        return regex.test(value);
-    });
-
-
-});
--- branches/5.0.0/admin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `#__rwcards`;
-DROP TABLE IF EXISTS `#__rwcardsdata`;
-DROP TABLE IF EXISTS `#__rwcardsconfig`;
-DROP TABLE IF EXISTS `#__rwcards_category`;
