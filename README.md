010cd00923f0c83b91d29f79a23537d1affba5c0
Ralf Weber <weber.ralf@blb-karlsruhe.de>
2021-05-21 11:30:10 +0200

    Updated Licence & README

--- README.md
+++ README.md
@@ -6,10 +6,16 @@ ECards Component for Joomla
 
 License
 =======
-Licensed under the terms of the LGPLv3.
+Licensed under the terms of CC-BY-SA
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons - This license allows reusers to distribute, remix, adapt, and build upon the material in any medium or format, so long as attribution is given to the creator. The license allows for commercial use. If you remix, adapt, or build upon the material, you must license the modified material under identical terms</a>.<br><br>
+                <strong>CC BY-SA includes the following elements:</strong>
+                <br><br>
+<strong>BY</strong> <img loading="lazy" class="alignnone " src="https://mirrors.creativecommons.org/presskit/icons/by.xlarge.png" width="30" height="30"><br>– Credit must be given to the creator
+<br><br>
+<strong>SA</strong> <img loading="lazy" class="alignnone " src="https://mirrors.creativecommons.org/presskit/icons/sa.xlarge.png" width="31" height="31"><br> – Adaptations must be shared under the same terms
 
 
 System requirements
 ===================
-<a href="http://www.joomla.org/" target="_blank">Jooml Open Source CMS</a><br>
+<a href="http://www.joomla.org/" target="_blank">Joomla Open Source CMS 3.X</a><br>
 Joomla is a free and open-source content management system (CMS) for publishing web content.
--- admin/helpers/rwcards.php
+++ admin/helpers/rwcards.php
@@ -26,6 +26,13 @@ abstract class RwcardsHelper
                 JSubMenuHelper::addEntry(JText::_('COM_RWCARDS_SUBMENU_UPLOAD'), 'index.php?option=com_rwcards&view=upload&extension=com_rwcards', $submenu == 'upload');
                 JSubMenuHelper::addEntry(JText::_('COM_RWCARDS_SUBMENU_SENT_CARDS'), 'index.php?option=com_rwcards&view=sentcards&extension=com_rwcards', $submenu == 'sentcards');
                 JSubMenuHelper::addEntry(JText::_('COM_RWCARDS_SUBMENU_ABOUT'), 'index.php?option=com_rwcards&view=about&extension=com_rwcards', $submenu == 'about');
+                JSubMenuHelper::addEntry('<br><br><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons Lizenzvertrag" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons - This license allows reusers to distribute, remix, adapt, and build upon the material in any medium or format, so long as attribution is given to the creator. The license allows for commercial use. If you remix, adapt, or build upon the material, you must license the modified material under identical terms</a>.<br><br>
+                <strong>CC BY-SA includes the following elements:</strong>
+                <br><br>
+<strong>BY</strong> <img loading="lazy" class="alignnone " src="https://mirrors.creativecommons.org/presskit/icons/by.xlarge.png" width="30" height="30"><br>– Credit must be given to the creator
+<br><br>
+<strong>SA</strong> <img loading="lazy" class="alignnone " src="https://mirrors.creativecommons.org/presskit/icons/sa.xlarge.png" width="31" height="31"><br> – Adaptations must be shared under the same terms');
+
                 // set some global property
                 $document = JFactory::getDocument();
                 $document->addStyleDeclaration('.icon-48-rwcards {background-image: url(../media/com_rwcards/images/rwcards.png);}');
